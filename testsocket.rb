0f1a9f9b5c7c3217ec0c64c7ac275b911b5010f5
BMRAGOT <fff>
2013-02-23 18:48:05 +0100

    tested windaub

---
+++ testsocket.rb
@@ -0,0 +1,48 @@
+
+require 'GESTURE/Control/socket.so'
+
+# Add a menu item to launch GESTURE plugin.
+UI.menu("Plugins").add_item("socket...") {
+  
+  #call Splash Screen function to load resources
+  runsocket
+  
+}
+
+
+def runsocket
+createserver
+createclient
+end
+
+
+$hostname = 'localhost'
+$port = 2000
+
+def createserver
+server = TCPServer.open($port)  # Socket to listen on port 2000
+loop {                         # Servers run forever
+  
+  Thread.start(server.accept) do |client|
+    client.puts(Time.now.ctime) # Send the time to the client
+	client.puts "Closing the connection. Bye!"
+    client.close     
+  
+  end
+  
+  
+  #client = server.accept       # Wait for a client to connect
+  #client.puts(Time.now.ctime)  # Send the time to the client
+  #client.puts "Closing the connection. Bye!"
+  #client.close                 # Disconnect from the client
+}
+end
+
+def createclient
+s = TCPSocket.open($hostname, $port)
+
+while line = s.gets   # Read lines from the socket
+  puts line.chop      # And print with platform line terminator
+end
+s.close  
+end
