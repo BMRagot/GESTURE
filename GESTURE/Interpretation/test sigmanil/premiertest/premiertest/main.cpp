ff313ef407d500c4eef2c4d3b6d076782b89f241
BMRagot <ddd>
2013-03-08 15:29:18 +0100

    add gesture recognition

--- GESTURE/Interpretation/test sigmanil/premiertest/premiertest/main.cpp
+++ GESTURE/Interpretation/test sigmanil/premiertest/premiertest/main.cpp
@@ -1,22 +1,23 @@
 //#define DllExport
-#include "Nil.h"
+#include "GESTURE_Recognition.h"
 #include <iostream>
 #include <string>
 //#define _SECURE_SCL 0 //-D_SCL_SECURE_NO_WARNINGS
 
-using namespace SigmaNIL; //Use SigmaNIL namespace to access library objects without the :: operator.
-
 
 int main(int argc, char *argv[])
 {
-
-	NIL nil;
-
-	nil.Init(OpenNI2);//
-
-	std::cout << nil.GetLastErrorString();
-	int i;
-	std::cin>> i ;
-
-	return 0;
+	std::cout << "Demarrage du programme \n";
+	//déclaration des variables
+	GESTURE_Recognition::GESTURE_Recognition() ;
 }
+
+
+
+	//std::cout << nil.GetLastErrorString();
+	
+	//int i;
+	//std::cin>> i ;
+
+	//return 0;
+//}
